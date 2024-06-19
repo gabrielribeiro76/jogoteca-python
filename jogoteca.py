f5091b86a3e155e0ea59e8bf5f11cf4bd6f5d03e
gabrielribeiro76 <gabrielribeirocontatos@gmail.com>
2024-06-19 14:52:44 -0300

    iniciando meu site em python com flask - jogoteca 1.0.0

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/home')
 def ola():
-    return 'Ola Mundo!'
+    return render_template('list.html')
 
 
 app.run()
